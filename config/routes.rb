cd44849bf73fda0af2c52fbe63922a4519d819cf
yurie-nishino <nishinoyurie0314@gmail.com>
2020-05-12 20:55:32 +0900

    device_marge

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'items#index'
+  root to: "home#index"
   resources :items, only: [:index, :show]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -88,10 +88,15 @@ ActiveRecord::Schema.define(version: 2020_05_12_020625) do
     t.date "birth_year", null: false
     t.date "birth_month", null: false
     t.date "birth_day", null: false
-    t.string "email", null: false
-    t.string "password", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "credit_cards", "users"
