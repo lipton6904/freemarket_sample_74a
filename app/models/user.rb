f25467cc092fdf7844c012f2454e1d75ce29c9ab
yurie-nishino <nishinoyurie0314@gmail.com>
2020-05-14 10:48:19 +0900

    device_haml_sass_controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,12 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :recoverable, :rememberable, :validatable
+
+  has_one :profile
+
+  validates :nickname, :encrypted_password, presence: true
+  validates :birthday, presence: true
+  validates :password, presence: true, length: { minimum: 7 }, format: { with: /\A[a-zA-Z0-9]+\z/, message: '半角で入力してください' }
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
+  devise_scope :user do
+    get 'profiles', to: 'users/registrations#new_profiles'
+    post 'profiles', to: 'users/registrations#create_profiles'
+  end
+  
   root 'items#index'
   resources :credit_cards, only:[:new]
+  resources :signup, only: [:new]
   resources :items, only: [:index, :new, :show] do
     resources :buys, only: [:show]
+    end
   end
-end
+
