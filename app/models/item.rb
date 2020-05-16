a660343153e137e46b60aeef73d659a9ed1c544a
lipton6904 <lipton6904@icloud.com>
2020-05-16 15:46:50 +0900

    Validation
    バリデーションの設定、マイグレーションファイルのコンフリクトの解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,15 +3,16 @@ class Item < ApplicationRecord
   accepts_nested_attributes_for :images
   belongs_to :category
   extend ActiveHash::Associations::ActiveRecordExtensions
-  # belongs_to_active_hash :prefecture
-  # belongs_to_active_hash :size
-  # belongs_to_active_hash :condition
-  # belongs_to_active_hash :derivery_fee
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :size
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :derivery_fee
   belongs_to_active_hash :shipping_area
-  # belongs_to_active_hash :days_untill_shippin
+  belongs_to_active_hash :days_untill_shipping
 
-  # validates :name,length: { in: 1..40 }
-  # validates :explanation,length: { in: 1..1000 }
-  # validates :price_id, numericality: { less_than_or_equal_to: 300, greater_than_or_equal_to: 9999999 }
-
+  validates :name,length: { in: 1..40 }
+  validates :explanation,length: { in: 1..1000 }
+  validates :price_id, numericality: { less_than_or_equal_to: 300, greater_than_or_equal_to: 9999999 }
+  validates :category_id, :condition_id, :derivery_fee_id,
+            :shipping_area_id, :days_untill_shipping_id, presence: true
 end
