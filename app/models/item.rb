c9932d40f9b7172b2ef4a0c39a899622a243a677
lipton6904 <lipton6904@icloud.com>
2020-05-21 14:28:10 +0900

    size_ Implementation
    サイズカテゴリの表示
    
    孫カテゴリがクリックされるとイベントが発火
    子カテゴリのIDによって表示されるサイズカテゴリの分岐を行なった。
    
    初めはhaml側でcssで表示、非表示を行なっていたが、カラム名が全て:size_idだったため、一番最後に書かれた内容しか反映されなかったため、js側でコントロールできるように実装を修正した。

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,6 @@ class Item < ApplicationRecord
   belongs_to :categorie
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
-  belongs_to_active_hash :size
   belongs_to_active_hash :condition
   belongs_to_active_hash :derivery_fee
   belongs_to_active_hash :shipping_area
