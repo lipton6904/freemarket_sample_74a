9754bb330b033c73105029b45f1949ee0d2d1d48
lipton6904 <lipton6904@icloud.com>
2020-05-22 19:43:10 +0900

    deploy_fix

--- app/assets/javascripts/payjp.js
+++ app/assets/javascripts/payjp.js
@@ -1,35 +1,35 @@
-window.addEventListener('DOMContentLoaded', function(){
+// window.addEventListener('DOMContentLoaded', function(){
 
-  let submit = document.getElementById("payment_card_submit-button");
+//   let submit = document.getElementById("payment_card_submit-button");
 
-  Payjp.setPublicKey('pk_test_4d7d704af23ffee45efd6847'); //公開鍵
+//   Payjp.setPublicKey('pk_test_4d7d704af23ffee45efd6847'); //公開鍵
 
-    submit.addEventListener('click', function(e){ //ボタン押すとトークン作成
+//     submit.addEventListener('click', function(e){ //ボタン押すとトークン作成
 
-    e.preventDefault(); //ボタンを無効
+//     e.preventDefault(); //ボタンを無効
 
-    let card = { //カード入力情報
-        number: document.getElementById("card_number").value,
-        cvc: document.getElementById("payment_card_cvc").value,
-        exp_month: document.getElementById("payment_card_month").value,
-        exp_year: document.getElementById("payment_card_year").value
-    };
+//     let card = { //カード入力情報
+//         number: document.getElementById("card_number").value,
+//         cvc: document.getElementById("payment_card_cvc").value,
+//         exp_month: document.getElementById("payment_card_month").value,
+//         exp_year: document.getElementById("payment_card_year").value
+//     };
 
-    Payjp.createToken(card, function(status, response) {  // トークン生成
-      if (status === 200) { 
-        //データをremoveAttr("name")で削除
-        $(".number").removeAttr("name");
-        $(".cvc").removeAttr("name");
-        $(".exp_month").removeAttr("name");
-        $(".exp_year").removeAttr("name"); 
-        $("#charge-form").append(
-          $('<input type="hidden" name="payjp_token">').val(response.id)
-        ); //取得したトークンを送信
-        document.inputForm.submit();
-        alert("登録が完了しました"); 
-      } else {
-        alert("カード情報が正しくありません。"); //エラー確認用
-      }
-    });
-  });
-});
+//     Payjp.createToken(card, function(status, response) {  // トークン生成
+//       if (status === 200) { 
+//         //データをremoveAttr("name")で削除
+//         $(".number").removeAttr("name");
+//         $(".cvc").removeAttr("name");
+//         $(".exp_month").removeAttr("name");
+//         $(".exp_year").removeAttr("name"); 
+//         $("#charge-form").append(
+//           $('<input type="hidden" name="payjp_token">').val(response.id)
+//         ); //取得したトークンを送信
+//         document.inputForm.submit();
+//         alert("登録が完了しました"); 
+//       } else {
+//         alert("カード情報が正しくありません。"); //エラー確認用
+//       }
+//     });
+//   });
+// });
