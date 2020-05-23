19213a09451a50d68a9aca7211912d729d95f9ea
yurie-nishino <nishinoyurie0314@gmail.com>
2020-05-23 20:11:43 +0900

    edit

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,9 +1,9 @@
 class BuysController < ApplicationController
   require 'payjp'
-  before_action :bye
+  before_action :check
   before_action :set_card
-
-  def bye
+  
+  def check
     if user_signed_in?
       set_card
     else
@@ -12,17 +12,16 @@ class BuysController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:item_id])
+    set_item
     @items = Item.includes(:images).order('created_at DESC')
   end
 
   def pay
     @item = Item.find(params[:item_id])
-    card = CreditCard.where(user_id: current_user.id).first
     Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
     Payjp::Charge.create(
       amount: @item.price_id, #支払金額
-      customer: card.customer_id, #顧客ID
+      customer: @card.customer_id, #顧客ID
       currency: 'jpy', #日本円
     )
     @item.buyer_id = current_user.id
@@ -34,5 +33,9 @@ class BuysController < ApplicationController
 
   def set_card
     @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
