77d2775955ac698fb5a865e972de31021917dbac
lipton6904 <lipton6904@icloud.com>
2020-05-23 19:06:41 +0900

    details_Implementation
    詳細画面の実装＋サイズの修正
    
    入力画面がログインしていない状態でも閲覧できるようにした。
    サイズがIDでしか出力でいなかったため、修正を行った。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,19 +5,19 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @category = Category.all.order("ancestry,id").limit(13)
+    @category = Categorie.all.order("ancestry,id").limit(13)
     @item = Item.new
     @item.images.new
   end
   
   def show
     @item = Item.find(params[:id])
-    # @items = Item.includes(:images).order('created_at DESC')
+    @items = Item.includes(:images).order('created_at DESC')
+    @user = User.find_by(id: @item.seller_id)
   end
 
   def create
     @item = Item.new(item_params)
-    binding.pry
     if  @item.save
       redirect_to root_path
     else
@@ -37,16 +37,18 @@ class ItemsController < ApplicationController
   end
 
   def category_children
-    @category_children = Category.find(params[:productcategory]).children 
+    @category_children = Categorie.find(params[:productcategory]).children 
   end
     
   def category_grandchildren
-    @category_grandchildren = Category.find(params[:productcategory]).children
+    @category_grandchildren = Categorie.find(params[:productcategory]).children
   end
+
+
 
 private
   def item_params
-    params.require(:item).permit(:name, :price_id, :explanation, :category_id, :size_id, :condition_id, :derivery_fee_id, :shipping_area_id, :days_untill_shipping_id, :status_id, images_attributes: [:image, :_destroy, :id]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price_id, :explanation, :category_id, :size_id, :condition_id, :derivery_fee_id, :shipping_area_id, :days_untill_shipping_id, images_attributes: [:image, :_destroy, :id]).merge(seller_id: current_user.id)
   end
   def set_item
     @item = Item.find(params[:id])
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,8 @@
 class Item < ApplicationRecord
   has_many :images,inverse_of: :item
   accepts_nested_attributes_for :images, allow_destroy: true
-  belongs_to :categorie
-  belongs_to :user
+  belongs_to :categorie, optional: true
+  belongs_to :user, optional: true
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :prefecture
   belongs_to_active_hash :condition
