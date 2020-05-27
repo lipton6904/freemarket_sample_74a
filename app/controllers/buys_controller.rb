d80242f75e717f825eb6f48b0053093ae1c738aa
Yuhei Nishi <abc-gtgtg.21q.abds@ezweb.ne.jp>
2020-05-27 19:46:51 +0900

    修正
    購入（エラーハンドリング）
    登録完了画面リンク先編集

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -10,29 +10,38 @@ class BuysController < ApplicationController
       redirect_to new_user_session_path
     end
   end
+
   def show
     @items = Item.includes(:images).order('created_at DESC')
   end
+
   def pay
     @item = Item.find(params[:item_id])
     @card = CreditCard.where(user_id: current_user.id).first
-    Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
-    Payjp::Charge.create(
-      amount: @item.price_id, #支払金額
-      customer: @card.customer_id, #顧客ID
-      currency: 'jpy', #日本円
-    )
-    @item.buyer_id = current_user.id
-    if @item.destroy
+    if @card.present?
+      Payjp.api_key = Rails.application.credentials.dig(:payjp, :PAYJP_SECRET_KEY)
+      Payjp::Charge.create(
+        amount: @item.price_id, #支払金額
+        customer: @card.customer_id, #顧客ID
+        currency: 'jpy', #日本円
+      )
+      @item.buyer_id = current_user.id
+      @item.destroy
       redirect_to root_path, alert: '購入しました'
     else
-      redirect_to root_path, alert: '購入できませんでした'
+      redirect_to new_credit_card_path,alert: 'クレジットカードの登録をしてください'
     end
+
+    @item.save
+    redirect_to root_path
   end
+
   private
+
   def set_card
     @card = CreditCard.where(user_id: current_user.id).first if CreditCard.where(user_id: current_user.id).present?
   end
+
   def set_item
     @item = Item.find(params[:id])
   end
